d6179db974dc632a1f79a8fdadfa26f4d34b16b4
bsawert <bsawert@skytouchtechnology.com>
2018-09-13 09:32:09 -0700

    More unit tests

--- src/test/java/com/sawert/swagger/service/BreedsServiceImplTest.java
+++ src/test/java/com/sawert/swagger/service/BreedsServiceImplTest.java
@@ -3,24 +3,32 @@ package com.sawert.swagger.service;
 import com.sawert.swagger.model.AKCGroup;
 import com.sawert.swagger.model.Breed;
 import com.sawert.swagger.repository.BreedDto;
+import com.sawert.swagger.repository.BreedMapper;
 import com.sawert.swagger.repository.BreedRepository;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 public class BreedsServiceImplTest {
@@ -30,7 +38,6 @@ public class BreedsServiceImplTest {
 
     @TestConfiguration
     static class BreedsServiceImplTestContextConfiguration {
-
         @Bean
         public BreedsService breedsService() {
             return new BreedsServiceImpl();
@@ -43,6 +50,14 @@ public class BreedsServiceImplTest {
     @MockBean
     private BreedRepository breedRepository;
 
+    private int numMockBreeds = 3;
+    private List<BreedDto> breedDtoList;
+
+    @Before
+    public void setup() {
+        setupMockRepository(this.numMockBreeds);
+    }
+
     @Test
     public void testAddBreed() {
         Breed breed = new Breed()
@@ -52,7 +67,7 @@ public class BreedsServiceImplTest {
         this.breedsService.addBreed(breed);
 
         ArgumentCaptor<BreedDto> argumentCaptor = ArgumentCaptor.forClass(BreedDto.class);
-        verify(breedRepository).save(argumentCaptor.capture());
+        verify(this.breedRepository).save(argumentCaptor.capture());
         BreedDto breedDto = argumentCaptor.getValue();
         assertThat(breedDto.getName().equals(breed.getName()));
         assertThat(breedDto.getDescription().equals(breed.getDescription()));
@@ -61,14 +76,33 @@ public class BreedsServiceImplTest {
 
     @Test
     public void testGetBreeds() {
+        List<Breed> breedList = this.breedsService.getBreeds();
+        assertThat(breedList).isNotNull();
+        assertThat(breedList.size()).isEqualTo(this.numMockBreeds);
+        assertThat(breedList.get(0)).isEqualTo(BreedMapper.toBreed(this.breedDtoList.get(0)));
     }
 
     @Test
-    public void testGetBreed() {
+    public void testGetBreedById() {
+        int index = new Random().nextInt(this.numMockBreeds);
+        Breed breed = this.breedsService.getBreed(1L + index);
+        assertThat(breed).isNotNull();
+        assertThat(breed.getId()).isEqualTo(1L + index);
+        assertThat(breed.getName()).isEqualTo(this.breedDtoList.get(index).getName());
+        assertThat(breed.getDescription()).isEqualTo(this.breedDtoList.get(index).getDescription());
+        assertThat(breed.getAkcgroup()).isEqualTo(this.breedDtoList.get(index).getAkcgroup());
     }
 
     @Test
     public void testGetBreedByName() {
+        int index = new Random().nextInt(this.numMockBreeds);
+        String name = this.breedDtoList.get(index).getName();
+        Breed breed = this.breedsService.getBreedByName(name);
+        assertThat(breed).isNotNull();
+        assertThat(breed.getId()).isEqualTo(1L + index);
+        assertThat(breed.getName()).isEqualTo(this.breedDtoList.get(index).getName());
+        assertThat(breed.getDescription()).isEqualTo(this.breedDtoList.get(index).getDescription());
+        assertThat(breed.getAkcgroup()).isEqualTo(this.breedDtoList.get(index).getAkcgroup());
     }
 
     @Test
@@ -77,5 +111,41 @@ public class BreedsServiceImplTest {
 
     @Test
     public void testGetBreedsByAkcGroups() {
+    }
+
+    private void setupMockRepository(int numBreeds) {
+        this.breedDtoList = new ArrayList();
+        for (int index = 1; index <= numBreeds; index++) {
+            BreedDto breedDto = new BreedDto("Breed " + index, "Breed " + index, AKCGroup.values()[index - 1]);
+            breedDto.setId(0L + index);
+            this.breedDtoList.add(breedDto);
+        }
+
+        when(this.breedRepository.findAll()).thenReturn(this.breedDtoList);
+
+        Answer<BreedDto> idAnswer = new Answer<BreedDto>() {
+            public BreedDto answer(InvocationOnMock invocation) throws Throwable {
+                Long id = invocation.getArgumentAt(0, Long.class);
+                return BreedsServiceImplTest.this.breedDtoList
+                        .stream()
+                        .filter(dto -> dto.getId().equals(id))
+                        .findFirst()
+                        .orElse(null);
+            }
+        };
+
+        when(this.breedRepository.findOne(anyLong())).thenAnswer(idAnswer);
+
+        Answer<List<BreedDto>> nameAnswer = new Answer<List<BreedDto>>() {
+            public List<BreedDto> answer(InvocationOnMock invocation) throws Throwable {
+                String name = invocation.getArgumentAt(0, String.class);
+                return BreedsServiceImplTest.this.breedDtoList
+                        .stream()
+                        .filter(dto -> dto.getName().equals(name))
+                        .collect(Collectors.toList());
+            }
+        };
+
+        when(this.breedRepository.findByName(anyString())).thenAnswer(nameAnswer);
     }
 }
