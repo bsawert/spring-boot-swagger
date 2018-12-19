cf0cb614466e327586ab4ee92a1b780e8f9bb7bd
bsawert <bsawert@skytouchtechnology.com>
2018-12-19 11:57:25 -0700

    Added H2 TCP server for external client

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -2,7 +2,7 @@ insert into breed_dto(id, name, description, akcgroup) values(1, 'Chihuahua', 'C
 insert into breed_dto(id, name, description, akcgroup) values(2, 'Rat Terrier', 'Rat Terrier', 'TERRIER');
 insert into breed_dto(id, name, description, akcgroup) values(3, 'Pug', 'Pug', 'TOY');
 insert into breed_dto(id, name, description, akcgroup) values(4, 'Blue Tick Hound', 'Blue Tick Coonhound', 'HOUND');
-insert into breed_dto(id, name, description, akcgroup) values(5, 'Mutt', 'Pound Puppy', 'MISCELLANEOUS');
+insert into breed_dto(id, name, description, akcgroup) values(5, 'Mixed Breed', 'Pound Puppy', 'MISCELLANEOUS');
 
 insert into dog_dto(id, name, description, gender) values(1, 'Puppy', 'Puppy', 'NEUTERED');
 insert into dog_breed (fk_dog, fk_breed) values(1, 1);
