b26ce5705e1d22a841e9df18480d2a873de978a8
bsawert <bsawert@skytouchtechnology.com>
2019-06-20 09:34:16 -0700

    Added tests for coverage

--- src/test/java/com/sawert/swagger/repository/BreedDtoTest.java
+++ src/test/java/com/sawert/swagger/repository/BreedDtoTest.java
@@ -1,6 +1,10 @@
 package com.sawert.swagger.repository;
 
-import com.sawert.swagger.model.AKCGroup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+import javax.persistence.PersistenceException;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,14 +14,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.persistence.PersistenceException;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.fail;
+import com.sawert.swagger.model.AKCGroup;
+import com.sawert.swagger.model.Breed;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class BreedDtoTest {
+    String name = "Mutt";
+    String description = "Pound puppy";
+    AKCGroup akcGroup = AKCGroup.MISCELLANEOUS;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -29,22 +34,22 @@ public class BreedDtoTest {
     public void createWhenNameIsNullShouldThrowException() {
         this.thrown.expect(IllegalArgumentException.class);
         this.thrown.expectMessage("Name must not be empty");
-        new BreedDto(null, "Pound puppy", AKCGroup.MISCELLANEOUS);
+        new BreedDto(null, description, akcGroup);
     }
 
     @Test
     public void createWhenNameIsEmptyShouldThrowException() {
         this.thrown.expect(IllegalArgumentException.class);
         this.thrown.expectMessage("Name must not be empty");
-        new BreedDto("", "Pound puppy", AKCGroup.MISCELLANEOUS);
+        new BreedDto("", description, akcGroup);
     }
 
     @Test
     public void saveShouldPersistData() {
         BreedDto dto = this.entityManager.persistFlushFind(
-            new BreedDto("Mutt", "Pound puppy", AKCGroup.MISCELLANEOUS));
-        assertThat(dto.getName()).isEqualTo("Mutt");
-        assertThat(dto.getDescription()).isEqualTo("Pound puppy");
+            new BreedDto(name, description, akcGroup));
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getDescription()).isEqualTo(description);
     }
 
     @Test
@@ -56,5 +61,33 @@ public class BreedDtoTest {
             new BreedDto("Mutt", "Another Pound puppy", AKCGroup.HOUND));
 
         fail("Unique constraint on name failed");
+    }
+
+    @Test
+    public void createWithBuilder() {
+        BreedDto dto = new BreedDto(name, description, akcGroup);
+        BreedDto dtoBuilt = BreedDto.builder()
+            .name(name)
+            .description(description)
+            .akcgroup(akcGroup)
+            .build();
+
+        assertThat(dtoBuilt.getName()).isEqualTo(dto.getName());
+        assertThat(dtoBuilt.getDescription()).isEqualTo(dto.getDescription());
+        assertThat(dtoBuilt.getAkcgroup()).isEqualTo(dto.getAkcgroup());
+    }
+
+    @Test
+    public void createFromBreed() {
+        Breed breed = new Breed();
+        breed.setName(name);
+        breed.setDescription(description);
+        breed.setAkcgroup(akcGroup);
+
+        BreedDto dto = new BreedDto(breed);
+
+        assertThat(dto.getName()).isEqualTo(breed.getName());
+        assertThat(dto.getDescription()).isEqualTo(breed.getDescription());
+        assertThat(dto.getAkcgroup()).isEqualTo(breed.getAkcgroup());
     }
 }
--- src/test/java/com/sawert/swagger/repository/DogDtoTest.java
+++ src/test/java/com/sawert/swagger/repository/DogDtoTest.java
@@ -1,5 +1,8 @@
 package com.sawert.swagger.repository;
 
+import com.sawert.swagger.model.AKCGroup;
+import com.sawert.swagger.model.Breed;
+import com.sawert.swagger.model.Dog;
 import com.sawert.swagger.model.Gender;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,12 +14,18 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.atIndex;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class DogDtoTest {
+    String name = "Fluffy";
+    String description = "Lap dog";
+    Gender gender = Gender.FEMALE;
+    Set<BreedDto> breedDtos = Collections.EMPTY_SET;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -28,21 +37,63 @@ public class DogDtoTest {
     public void createWhenNameIsNullShouldThrowException() {
         this.thrown.expect(IllegalArgumentException.class);
         this.thrown.expectMessage("Name must not be empty");
-        new DogDto(null, "Lap dog", Gender.FEMALE, Collections.<BreedDto>emptySet());
+        new DogDto(null, description, gender, breedDtos);
     }
 
     @Test
     public void createWhenNameIsEmptyShouldThrowException() {
         this.thrown.expect(IllegalArgumentException.class);
         this.thrown.expectMessage("Name must not be empty");
-        new DogDto("", "Lap dog", Gender.FEMALE, Collections.<BreedDto>emptySet());
+        new DogDto("",  description, gender, breedDtos);
     }
 
     @Test
     public void saveShouldPersistData() {
         DogDto dto = this.entityManager.persistFlushFind(
-            new DogDto("Fluffy", "Lap dog", Gender.FEMALE, Collections.<BreedDto>emptySet()));
-        assertThat(dto.getName()).isEqualTo("Fluffy");
-        assertThat(dto.getDescription()).isEqualTo("Lap dog");
+            new DogDto(name, description, gender, breedDtos));
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getDescription()).isEqualTo(description);
+        assertThat(dto.getGender()).isEqualTo(gender);
+        assertThat(dto.getBreedDtos()).isEqualTo(breedDtos);
+    }
+
+    @Test
+    public void createWithPartialConstructor() {
+        DogDto dto = new DogDto(name, description, gender, Collections.EMPTY_SET);
+        DogDto dtoPartial = new DogDto(name, description, gender);
+
+        assertThat(dtoPartial).isEqualTo(dto);
+    }
+
+    @Test
+    public void createWithBuilder() {
+        DogDto dto = new DogDto(name, description, gender, breedDtos);
+        DogDto dtoBuilt = DogDto.builder()
+                .name(name)
+                .description(description)
+                .gender(gender)
+                .breedDtos(breedDtos)
+                .build();
+
+        assertThat(dtoBuilt.getName()).isEqualTo(dto.getName());
+        assertThat(dtoBuilt.getDescription()).isEqualTo(dto.getDescription());
+        assertThat(dtoBuilt.getGender()).isEqualTo(dto.getGender());
+        assertThat(dtoBuilt.getBreedDtos()).isEqualTo(dto.getBreedDtos());
+    }
+
+    @Test
+    public void createFromBreed() {
+        Dog dog = new Dog();
+        dog.setName(name);
+        dog.setDescription(description);
+        dog.setGender(gender);
+        dog.setBreed(Collections.EMPTY_LIST);
+
+        DogDto dto = new DogDto(dog);
+
+        assertThat(dto.getName()).isEqualTo(dog.getName());
+        assertThat(dto.getDescription()).isEqualTo(dog.getDescription());
+        assertThat(dto.getGender()).isEqualTo(dog.getGender());
+        assertThat(dto.getBreedDtos()).isEmpty();
     }
 }
