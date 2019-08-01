a0c73f01b3a4b0f82d44e9b01040e16b7d28bfeb
bsawert <bsawert@gmail.com>
2019-08-01 16:20:58 +0000

    Generated code by SwaggerHub

---
+++ spring/src/main/java/com/sawert/swagger/api/DogsApiController.java
@@ -0,0 +1,20 @@
+package com.sawert.swagger.api;
+
+import org.springframework.stereotype.Controller;
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-08-01T16:20:58.572Z")
+
+@Controller
+public class DogsApiController implements DogsApi {
+
+    private final DogsApiDelegate delegate;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public DogsApiController(DogsApiDelegate delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public DogsApiDelegate getDelegate() {
+        return delegate;
+    }
+}
---
+++ spring/src/main/java/com/sawert/swagger/model/Gender.java
@@ -0,0 +1,51 @@
+package com.sawert.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.io.Serializable;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+/**
+ * Gets or Sets Gender
+ */
+public enum Gender {
+  
+  MALE("Male"),
+  
+  FEMALE("Female"),
+  
+  MALE_NEUTERED("Male_Neutered"),
+  
+  FEMALE_NEUTERED("Female_Neutered"),
+  
+  OTHER("Other"),
+  
+  UNKNOWN("Unknown");
+
+  private String value;
+
+  Gender(String value) {
+    this.value = value;
+  }
+
+  @Override
+  @JsonValue
+  public String toString() {
+    return String.valueOf(value);
+  }
+
+  @JsonCreator
+  public static Gender fromValue(String text) {
+    for (Gender b : Gender.values()) {
+      if (String.valueOf(b.value).equals(text)) {
+        return b;
+      }
+    }
+    return null;
+  }
+}
+
