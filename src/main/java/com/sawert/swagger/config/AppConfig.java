730c656329919b80c6feb3ca0ef9f7cb75df6dc3
root <root@a50ba9af3805>
2019-08-01 16:24:37 +0000

    Removed files managed by codegen

--- src/main/java/com/sawert/swagger/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package com.sawert.swagger.config;
-
-//@SpringBootConfiguration
-////@Import({RepositoryConfig.class, ServiceConfig.class})
-//@EnableAutoConfiguration
-//@ComponentScan
-public class AppConfig {
-}
